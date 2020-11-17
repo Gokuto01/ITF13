dea2079521878657dd13242c26ed2c654a977e5b
Gokuto Kung <73678927+Gokuto01@users.noreply.github.com>
2020-11-17 19:00:57 +0700

    Add files via upload

--- delete.php
+++ delete.php
@@ -1,7 +1,7 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'panwit.mysql.database.azure.com', 'panwit@panwit', '123456789', 'itflab', 3306);
+mysqli_real_connect($conn, 'itf13.mysql.database.azure.com', 'Gokuto@itf13', 'Golf1112', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
