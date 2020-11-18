30176861acf0639d6bd417600ae309ee6f2bde15
Gokuto Kung <73678927+Gokuto01@users.noreply.github.com>
2020-11-18 20:42:37 +0700

    Add files via upload

--- insert.php
+++ insert.php
@@ -35,7 +35,9 @@ if (mysqli_query($conn, $sql)) {
   
 mysqli_close($conn);
 ?>
+<div>
 <input class="btn btn-success" type="button" value="กลับหน้าหลัก" onclick="window.location.href='https://itf13.azurewebsites.net/show.php'" />
+</div>
 </div>
 </div>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
