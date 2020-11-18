67a8a38e11ad74c6a0cdfaae29ca7e89ec9acfc5
Gokuto Kung <73678927+Gokuto01@users.noreply.github.com>
2020-11-18 19:44:43 +0700

    Add files via upload

--- show.php
+++ show.php
@@ -3,7 +3,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 <title>ITF Lab</title>
 </head>
-<body>
+<body class="text-center">
 <?php
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'itf13.mysql.database.azure.com', 'Gokuto@itf13', 'Golf1112', 'itflab', 3306);
